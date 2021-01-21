3e4a824636420450e756b35bf0f48a5b18612e5d
BrianBell422 <brianbell.uk@gmail.com>
2021-01-21 19:29:04 +0000

    Started Football

--- start_code/football_results/src/football_results.py
+++ start_code/football_results/src/football_results.py
@@ -1,6 +1,12 @@
-def get_result(final_score):
-    pass
 
-def get_results(final_scores):
-    pass
-    # (You could try and use a list comprehension for this)
+
+
+
+
+
+
+
+# def get_result(final_score):
+    
+
+# (You could try and use a list comprehension for this)
--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -2,7 +2,18 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.home_win = {"home_score": 3, "away_score": 2}
+        self.away_win =  {"home_score": 0, "away_score": 2}
+        self.draw = {"home_score": 1, "away_score": 1}
+        self.final_scores = [self.home_win, self.away_win, self.draw]
+
+
+    
+    
+    
+    
     # Test we get the right result string for a final score dictionary representing -
 
         # Home win
