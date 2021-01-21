4ab81669b5d49e6f4ce49f8170f9732383a0c612
BrianBell422 <brianbell.uk@gmail.com>
2021-01-21 16:32:41 +0000

    High scores

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -1,10 +1,23 @@
-def latest(scores):
-    pass
+class HighScores:
+
+    def __init__(self, scores):
+        self.scores = scores
+
+    def latest_score(self):
+        return self.scores.pop(-1)
+  
+    def personal_best(self):
+        return max(self.scores)
+    
+    
+    def personal_top_three(self):
+        self.scores.sort()
+        return self.scores[-3:]
+
+    def highest_to_lowest(self):
+        self.scores.sort()
+        return self.scores.reverse()
 
 
-def personal_best(scores):
-    pass
 
-
-def personal_top_three(scores):
-    pass
+    
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,25 +1,43 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import HighScores
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoresTest(unittest.TestCase):
+
+    def setUp(self):
+        self.score_list = HighScores([10, 15, 18, 20, 30])
     
     # Tests
+    def test_has_scores(self):
+        self.assertEqual([10, 15, 18, 20, 30], self.score_list.scores)
 
     # Test latest score (the last thing in the list)
+    def test_latest_score(self):
+        self.assertEqual(30, self.score_list.latest_score())
 
     # Test personal best (the highest score in the list)
+    def test_personal_best_scores(self):
+        self.assertEqual(30, self.score_list.personal_best())
+
 
     # Test top three from list of scores
+    def test_personal_top_three(self):
+        self.assertEqual([18, 20, 30], self.score_list.personal_top_three())
 
-    # Test ordered from highest tp lowest
+    # Test ordered from highest to lowest
+    def test_highest_to_lowest(self):
+        self.assertEqual([30, 20, 18, 15, 10], self.score_list.highest_to_lowest())
 
     # Test top three when there is a tie
 
     # Test top three when there are less than three
 
     # Test top three when there is only one
+
+
+
+    #
     
